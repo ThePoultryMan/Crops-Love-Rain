afdadd1858e6ef87cb16dd899d3c27a600879b27
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-02-13 23:13:21 -0500

    Add rain crop growth speed up system

--- src/main/java/thepoultryman/crops_love_rain/CropsLoveRain.java
+++ src/main/java/thepoultryman/crops_love_rain/CropsLoveRain.java
@@ -1,15 +1,19 @@
 package thepoultryman.crops_love_rain;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CropsLoveRain implements ModInitializer {
-	public static final String MOD_ID = "modid";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger("crops_love_rain");
+
+	public static final GameRules.Key<GameRules.IntRule> CROP_GROWTH_SPEED_DURING_RAIN = GameRuleRegistry.register("cropGrowthSpeedDuringRain", GameRules.Category.UPDATES, GameRuleFactory.createIntRule(10));
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initializing [inset mod name here]");
+		LOGGER.info("Initializing rain onto crops");
 	}
 }
--- src/main/java/thepoultryman/crops_love_rain/mixin/CropsLoveRainMixin.java
+++ src/main/java/thepoultryman/crops_love_rain/mixin/CropsLoveRainMixin.java
@@ -1,16 +1,41 @@
 package thepoultryman.crops_love_rain.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CropBlock;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import thepoultryman.crops_love_rain.CropsLoveRain;
 
-@Mixin(TitleScreen.class)
-public class CropsLoveRainMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		CropsLoveRain.LOGGER.info("This line is printed by an example mod mixin!");
+import java.util.Random;
+
+@Mixin(CropBlock.class)
+public abstract class CropsLoveRainMixin {
+	@Shadow public abstract BlockState withAge(int age);
+
+	@Shadow protected abstract int getAge(BlockState state);
+
+	@Shadow
+	protected static float getAvailableMoisture(Block block, BlockView world, BlockPos pos) {
+		return 0;
 	}
+
+	@Inject(at = @At("HEAD"), method = "randomTick")
+	public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+		if (world.getBaseLightLevel(pos, 0) >= 9 && world.isRaining()) {
+			Block cropBlock = world.getBlockState(pos).getBlock();
+			int rainGrowthSpeed = world.getGameRules().getInt(CropsLoveRain.CROP_GROWTH_SPEED_DURING_RAIN);
+			if (random.nextInt((int) (rainGrowthSpeed / getAvailableMoisture(cropBlock, world, pos)) + 1) == 0) {
+				world.setBlockState(pos, this.withAge(this.getAge(state) + 1), 2);
+			}
+		}
+	}
+
 }
