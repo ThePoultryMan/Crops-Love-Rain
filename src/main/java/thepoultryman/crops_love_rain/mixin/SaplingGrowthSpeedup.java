e6c2a0d34e30702b21c1b33fb242f369626b36f3
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-07 15:05:18 -0400

    Merge branch '1.19/dev' into main

--- src/main/java/thepoultryman/crops_love_rain/mixin/SaplingGrowthSpeedup.java
+++ src/main/java/thepoultryman/crops_love_rain/mixin/SaplingGrowthSpeedup.java
@@ -4,6 +4,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SaplingBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.random.RandomGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,11 +16,10 @@ import java.util.Random;
 
 @Mixin(SaplingBlock.class)
 public abstract class SaplingGrowthSpeedup {
-
-    @Shadow public abstract void generate(ServerWorld world, BlockPos pos, BlockState state, Random random);
+    @Shadow public abstract void generate(ServerWorld world, BlockPos pos, BlockState state, RandomGenerator random);
 
     @Inject(at = @At("TAIL"), method = "randomTick")
-    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, RandomGenerator random, CallbackInfo ci) {
         int rainGrowthSpeed = world.getGameRules().getInt(CropsLoveRain.CROP_GROWTH_SPEED_DURING_RAIN);
         if (rainGrowthSpeed != 0 && world.hasRain(pos) && random.nextInt(rainGrowthSpeed / 2) == 0) {
             generate(world, pos, state, random);
