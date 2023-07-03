4ee5bba79ed857decca99f0a926fa0438278e235
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-07-03 15:51:20 -0400

    Bamboo & Sugar Cane support

--- src/main/java/thepoultryman/crops_love_rain/CropsLoveRain.java
+++ src/main/java/thepoultryman/crops_love_rain/CropsLoveRain.java
@@ -3,17 +3,25 @@ package thepoultryman.crops_love_rain;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.util.random.RandomGenerator;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CropsLoveRain implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("crops_love_rain");
 
-	public static final GameRules.Key<GameRules.IntRule> CROP_GROWTH_SPEED_DURING_RAIN = GameRuleRegistry.register("cropGrowthSpeedDuringRain", GameRules.Category.UPDATES, GameRuleFactory.createIntRule(10));
+	public static final GameRules.Key<GameRules.IntRule> CROP_GROWTH_SPEED_DURING_RAIN = GameRuleRegistry
+			.register("cropGrowthSpeedDuringRain", GameRules.Category.UPDATES, GameRuleFactory.createIntRule(10));
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing rain onto crops");
+	}
+
+	public static boolean shouldGrowExtra(World world, RandomGenerator random) {
+		int rainGrowthSpeed = world.getGameRules().getInt(CROP_GROWTH_SPEED_DURING_RAIN);
+		return random.nextInt((rainGrowthSpeed * 10) / 2) == 0;
 	}
 }
---
+++ src/main/java/thepoultryman/crops_love_rain/mixin/bamboo/GrowthSpeedup.java
@@ -0,0 +1,37 @@
+package thepoultryman.crops_love_rain.mixin.bamboo;
+
+import net.minecraft.block.BambooBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.random.RandomGenerator;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import thepoultryman.crops_love_rain.CropsLoveRain;
+
+@Mixin(BambooBlock.class)
+public abstract class GrowthSpeedup {
+    @Shadow @Final public static IntProperty STAGE;
+
+    @Shadow protected abstract int countBambooBelow(BlockView world, BlockPos pos);
+
+    @Shadow protected abstract void updateLeaves(BlockState state, World world, BlockPos pos, RandomGenerator random, int height);
+
+    @Inject(at = @At("HEAD"), method = "randomTick")
+    public void crops_love_rain$extraBlockTick(BlockState state, ServerWorld world, BlockPos pos, RandomGenerator random, CallbackInfo ci) {
+        if (!world.hasRain(pos)) return;
+        if (state.get(STAGE) == 0 && world.isAir(pos.up()) && world.getBaseLightLevel(pos.up(), 0) >= 9 && CropsLoveRain.shouldGrowExtra(world, random)) {
+            int bambooBelow = countBambooBelow(world, pos) + 1;
+            if (bambooBelow > 16) {
+                this.updateLeaves(state, world, pos, random, bambooBelow);
+            }
+        }
+    }
+}
