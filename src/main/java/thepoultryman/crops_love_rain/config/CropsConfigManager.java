a75fc9b9d5ec40ee7b0444b1a21713da12eab68a
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-07-06 22:08:56 -0400

    All config options (unused)

--- src/main/java/thepoultryman/crops_love_rain/config/CropsConfigManager.java
+++ src/main/java/thepoultryman/crops_love_rain/config/CropsConfigManager.java
@@ -3,6 +3,7 @@ package thepoultryman.crops_love_rain.config;
 import io.github.thepoultryman.cactusconfig.ConfigManager;
 import io.github.thepoultryman.cactusconfig.OptionHolder;
 import io.github.thepoultryman.cactusconfig.Options;
+import io.github.thepoultryman.cactusconfig.util.CactusUtil;
 import net.minecraft.text.Text;
 
 public class CropsConfigManager extends ConfigManager {
@@ -12,7 +13,30 @@ public class CropsConfigManager extends ConfigManager {
     public boolean useRainGrowthSpeed;
     @Options.Integer(tab = "general", defaultValue = 10)
     public int rainGrowthSpeed;
-
+    @Options.OptionHolder
+    public final OptionHolder individual = new OptionHolder(
+            Text.translatable("crops_love_rain.config.individual"), Text.translatable("crops_love_rain.config.individual.desc")
+    );
+    @Options.Boolean(tab = "individual", defaultValue = false)
+    public boolean useIndividualSpeeds;
+    @Options.Separator(tab = "individual")
+    private final CactusUtil.ConfigOption individualSeparator = new CactusUtil.ConfigOption(true, false);
+    @Options.Boolean(tab = "individual")
+    public boolean useGeneralSpeedBamboo;
+    @Options.Integer(tab = "individual")
+    public int bambooCustomSpeed;
+    @Options.Boolean(tab = "individual")
+    public boolean useGeneralSpeedCrops;
+    @Options.Integer(tab = "individual")
+    public int cropsCustomSpeed;
+    @Options.Boolean(tab = "individual")
+    public boolean useGeneralSpeedSaplings;
+    @Options.Integer(tab = "individual")
+    public int saplingCustomSpeed;
+    @Options.Boolean(tab = "individual")
+    public boolean useGeneralSpeedSugarCane;
+    @Options.Integer(tab = "individual")
+    public int sugarCaneCustomSpeed;
 
     @Override
     protected void load() {
